5d0f4e04b8854b22238ff7d774d681cfa45bcf19
marcia yurie yano <83242742+marciayano@users.noreply.github.com>
2021-10-21 21:56:48 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const LinksSocialMedia = {
+  github: 'marciayano',
+  facebook: 'marcia.yurie',
+  instagram: 'yu.marcia',
+  twitter: 'ponkanyu'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${LinksSocialMedia[github]}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login })}
+
+getGitHubProfileInfos()
